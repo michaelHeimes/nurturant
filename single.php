a996a60e935278729ee141c885ddec15c7c47482
Michael Heimes <michael@cairndigitalmedia.com>
2023-11-06 13:59:35 -0500

    blog

--- single.php
+++ single.php
@@ -11,30 +11,28 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'nurturant' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'nurturant' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+		<div class="content">
+			
+			<div class="inner-content grid-x grid-padding-x">
+			
+				<main class="main cell small-12 medium-12 large-8 large-offset-2" role="main">
+			
+					<?php
+					while ( have_posts() ) :
+						the_post();
+			
+						get_template_part( 'template-parts/content', get_post_type() );
+						
+					endwhile; // End of the loop.
+					?>
+					
+				</main>
+				
+			</div>
+			
+		</div>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
